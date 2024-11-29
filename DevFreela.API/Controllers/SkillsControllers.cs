db4a889b51c92d684b0efb5e5b92e37c414a479a
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-29 18:42:53 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.API/Controllers/SkillsControllers.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/skills")]
+    [ApiController]
+    public class SkillsControllers:ControllerBase
+    {
+    }
+}
---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Controller]
+    [Route("api/users")]
+    public class UsersController:ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
