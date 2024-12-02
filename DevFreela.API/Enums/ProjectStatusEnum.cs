44395440f9c6020b4f835c5e0065d3198792be25
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-02 17:43:32 -0300

    criando enum de status no projeto

---
+++ DevFreela.API/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace DevFreela.API.Enums
+{
+    public enum ProjectStatusEnum
+    {
+        created = 0,
+        InProgress = 1,
+        Suspended = 2,
+        cancelled = 3,
+        Completed = 4,
+        peymentPending=5
+
+    }
+}
