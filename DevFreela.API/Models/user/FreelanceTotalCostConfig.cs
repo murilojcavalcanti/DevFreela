39a0877e22cdabfbfb3c41c353d40cc6aba362e7
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-02 18:56:50 -0300

    Criando models da classe user

---
+++ DevFreela.API/Models/user/FreelanceTotalCostConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace DevFreela.API.Models.user
+{
+    public class FreelanceTotalCostConfig
+    {
+        public decimal Minimum { get; set; }
+        public decimal Maxmum { get; set; }
+    }
+}
