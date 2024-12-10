994aec5cc2d1c3b1ae75fd5f3fc2a8677961e9d8
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-10 15:29:17 -0300

    criando devfreela.core

---
+++ DevFreela.Core/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedAt = DateTime.Now;
+            IsDeleted = false;
+        }
+        public int Id { get; private set; }
+        public DateTime CreatedAt {get; private set; }
+        public bool IsDeleted {get; private set; }
+
+        public void SetAsDeleted()
+        {
+            IsDeleted = true;
+        }
+    }
+}
---
+++ DevFreela.Core/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace DevFreela.Core.Enums
+{
+    public enum ProjectStatusEnum
+    {
+        created = 0,
+        InProgress = 1,
+        Suspended = 2,
+        cancelled = 3,
+        Completed = 4,
+        peymentPending=5
+
+    }
+}
