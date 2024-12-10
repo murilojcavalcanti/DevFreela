498887787b0bf02b9ebf8bc67c4d73c491203623
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-10 19:14:05 -0300

    Criando extension Methods

--- DevFreela.Application/Models/Skills/CreateSkillInputModel.cs
+++ DevFreela.Application/Models/Skills/CreateSkillInputModel.cs
@@ -1,7 +1,11 @@
-﻿namespace DevFreela.Infrastructure.Models.Skills
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Infrastructure.Models.Skills
 {
     public class CreateSkillInputModel
     {
         public string description { get; set; }
+        public Skill ToEntity() => new Skill(description); 
     }
-}
+ }
+
---
+++ DevFreela.Application/Models/Skills/SkillViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Models.user;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Models.Skills
+{
+    public class SkillViewModel
+    {
+        public SkillViewModel(string description)
+        {
+            Description = description;
+        }
+        public string Description { get; set; }
+
+
+        public static SkillViewModel FromEntity(Skill skill)
+        {
+            return new SkillViewModel(skill.Description);
+        }
+    }
+}
