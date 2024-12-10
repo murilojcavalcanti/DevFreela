d9d60681e21728dc17009f5da21d41b3d124ca9d
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-10 15:28:56 -0300

    criando application module

---
+++ DevFreela.Application/ApplicationModule.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application
+{
+    //Esta classe será o unico ponto de contato entre o projeto application e o projeto api.
+    //Ao inves de adicionar todos os sevices do projeto na classe program, este arquivo será responsavel por isso.
+    public static class ApplicationModule
+    {
+        
+
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddServices();
+            return services;
+
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services) 
+        {
+            services.AddScoped<IProjectService, ProjectService>();
+            return services;
+        }
+    }
+}
