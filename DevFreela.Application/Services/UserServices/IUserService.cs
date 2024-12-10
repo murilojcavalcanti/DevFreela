31b377cb70c2063158089a1c837c48fcd9b63913
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-10 18:08:03 -0300

    Criando interface IUserService

---
+++ DevFreela.Application/Services/UserServices/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Models.Skills;
+using DevFreela.Infrastructure.Models.user;
+using DevFreela.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.UserServices
+{
+    internal interface IUserService
+    {
+        ResultViewModel<int> Insert(CreateUserInputModel model);
+        ResultViewModel<List<UserViewModel>> GetAll(string search = "",int size = 5,int page = 0);
+        ResultViewModel<UserViewModel> GetById(int model);
+        ResultViewModel InsertSkills(int id, UserSkillsInputModel model);
+    }
+}
