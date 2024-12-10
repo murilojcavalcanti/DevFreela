f4e726eabd8a415f79270b5f47bad5c6aeee4caa
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-10 18:32:45 -0300

    Refatorando a controller para usar os services

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -5,54 +5,50 @@ using DevFreela.Infrastructure.Persistence;
 using DevFreela.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using DevFreela.Application.Services.UserServices;
 namespace DevFreela.API.Controllers
 {
     [Controller]
     [Route("api/users")]
     public class UsersController:ControllerBase
     {
-        private readonly DevFreelaDbContext _context;
+        private readonly IUserService _service;
 
-        public UsersController(DevFreelaDbContext context)
+        public UsersController(IUserService service)
         {
-            _context = context;
+            _service = service;
         }
 
         [HttpPost]
         public IActionResult Post(CreateUserInputModel model)
         {
-            User user = model.ToEntity();
-            _context.Users.Add(user);
-            _context.SaveChanges();
-            return Ok(user);
+            var result = _service.Insert(model);
+            if(!result.IsSuccess) return BadRequest(result.Data);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data }, model);
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string search = "", int size = 5, int page =0 )
         {
-            var users = _context.Users
-                .Include(u => u.Skills)
-                .ThenInclude(u => u.Skill).ToList();
-            
-            return Ok(users);
+            var result = _service.GetAll(search,size,page);
+            if (!result.IsSuccess) return NotFound(result.Data);
+            return Ok(result);
         }
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-           var user = _context.Users
-                .Include(u=>u.Skills)
-                .ThenInclude(u=>u.Skill)
-                .SingleOrDefault(u=>u.Id==id);
-            return Ok(user);
+           var result = _service.GetById(id);
+            if (!result.IsSuccess) return NotFound(result.Data);
+            return Ok(result);
         }
 
         [HttpPost("{id}/skills")]
         public IActionResult PostSkills(int id, UserSkillsInputModel model)
         {
-            var UserSkills = model.SkillIds.Select(s => new UserSkill(id, s)).ToList();
-            _context.UserSkills.AddRange(UserSkills);
-            return Ok();
+            var result = _service.InsertSkills(id,model);
+            if (!result.IsSuccess) return BadRequest(result.Message);
+            return Ok(result);
         }
     }
 }
