ac27de9e3915bfd2504a182e72e8df1b55f6b7b7
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-12 18:39:07 -0300

    começando a implementar o CQRS, usando   MediatR

---
+++ DevFreela.Application/Services/Commands/CommandsProject/CompleteProject/CompleteProjectCommand.cs
@@ -0,0 +1,21 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.CompleteProject
+{
+    public class CompleteProjectCommand: IRequest<ResultViewModel>
+    {
+
+        public CompleteProjectCommand(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandsProject/CompleteProject/CompleteProjectHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.CompleteProject
+{
+    public class CompleteProjectHandler : IRequestHandler<CompleteProjectCommand, ResultViewModel>
+    {
+
+        private readonly DevFreelaDbContext _context;
+
+        public CompleteProjectHandler(DevFreelaDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public async Task<ResultViewModel> Handle(CompleteProjectCommand request, CancellationToken cancellationToken)
+        {
+            Project project = await _context.Projects.SingleOrDefaultAsync(p => p.Id ==request.Id);
+            
+            if (project is null) return ResultViewModel.Error("Projeto não existe!");
+            
+            project.Complete();
+            _context.Projects.Update(project);
+            _context.SaveChangesAsync();
+            return ResultViewModel.Success();
+        }
+    }
+}
