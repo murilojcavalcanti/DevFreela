4f28f9966a023b0f6f5b3ff037bf251b00d44caa
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-12 18:40:13 -0300

    Implementando InsertComment

---
+++ DevFreela.Application/Services/Commands/CommandsProject/InsertCommentProject/InsertCommentProjectCommand.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.InsertCommentProject
+{
+    public class InsertCommentProjectCommand:IRequest<ResultViewModel>
+    {
+        public string Content { get; set; }
+        public int IdProject { get; set; }
+        public int IdUser { get; set; }
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandsProject/InsertCommentProject/InsertCommentProjectHandler.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.InsertCommentProject
+{
+    internal class InsertCommentProjectHandler : IRequestHandler<InsertCommentProjectCommand, ResultViewModel>
+    {
+
+        private readonly DevFreelaDbContext _context;
+
+        public InsertCommentProjectHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(InsertCommentProjectCommand request, CancellationToken cancellationToken)
+        {
+            Project project = await _context.Projects.SingleOrDefaultAsync(p => p.Id == request.IdProject);
+            if (project is null) return ResultViewModel.Error("Projeto não existe!");
+
+            var comment = new ProjectComment(request.Content, request.IdProject, request.IdUser);
+            _context.ProjectComments.AddAsync(comment);
+            _context.SaveChangesAsync();
+
+            return ResultViewModel.Success();
+
+        }
+    }
+}
