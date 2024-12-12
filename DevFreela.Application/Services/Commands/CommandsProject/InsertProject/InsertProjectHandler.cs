c6e00790f73237013c763395475c02994aa1e34f
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-12 18:40:47 -0300

    Implementando InsertProject

---
+++ DevFreela.Application/Services/Commands/CommandsProject/InsertProject/InsertProjectHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.InsertProject
+{
+    internal class InsertProjectHandler : IRequestHandler<InsertProjectCommand, ResultViewModel<int>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public InsertProjectHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(InsertProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = request.ToEntity();
+            _context.Projects.AddAsync(project);
+            _context.SaveChangesAsync();
+            return ResultViewModel<int>.Success(project.Id);
+        }
+    }
+}
