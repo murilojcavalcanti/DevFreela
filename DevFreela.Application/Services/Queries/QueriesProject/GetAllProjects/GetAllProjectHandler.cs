a36cb2a46157c892d2ab20e2643d259ccbc2b174
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-12 18:42:09 -0300

    Implementando GetAll

---
+++ DevFreela.Application/Services/Queries/QueriesProject/GetAllProjects/GetAllProjectHandler.cs
@@ -0,0 +1,41 @@
+﻿using Azure;
+using DevFreela.Application.Models;
+using DevFreela.Application.Models.project;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesProject.GetAllProjects
+{
+    public class GetAllProjectHandler : IRequestHandler<GetAllProjectQuery, ResultViewModel<List<ProjectItemViewModel>>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetAllProjectHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<List<ProjectItemViewModel>>> Handle(GetAllProjectQuery request, CancellationToken cancellationToken)
+        {
+            var projects = await _context.Projects
+            .Include(p => p.Client)
+            .Include(p => p.Freelancer)
+            .Where(p => !p.IsDeleted &&
+                (request.Search == "" ||
+                p.Title.Contains(request.Search)))
+                .Skip(request.Page * request.Size)
+                .Take(request.Size)
+            .ToListAsync();
+            List<ProjectItemViewModel> model = projects.Select(p => ProjectItemViewModel.FromEntity(p)).ToList();
+            return ResultViewModel<List<ProjectItemViewModel>>.Success(model);
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Queries/QueriesProject/GetAllProjects/GetAllProjectQuery.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Application.Models.project;
+using DevFreela.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesProject.GetAllProjects
+{
+    public class GetAllProjectQuery : IRequest<ResultViewModel<List<ProjectItemViewModel>>>
+    {
+        public string Search { get; set; }
+        public int Size { get; set; }
+        public int Page { get; set; }
+    }
+}
