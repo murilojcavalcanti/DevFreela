b86541389980c70d4855062a63122cfcdb08e4c4
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-12 18:39:33 -0300

    Implementando Delete

---
+++ DevFreela.Application/Services/Commands/CommandsProject/DeleteProject/DeleteProjectCommand.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.DeleteProject
+{
+    public class DeleteProjectCommand:IRequest<ResultViewModel>
+    {
+        public DeleteProjectCommand(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandsProject/DeleteProject/DeleteProjectHandler.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.DeleteProject
+{
+    public class DeleteProjectHandler : IRequestHandler<DeleteProjectCommand, ResultViewModel>
+    {
+        private DevFreelaDbContext _context;
+
+        public DeleteProjectHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
+        {
+            Project project =await  _context.Projects.SingleOrDefaultAsync(p => p.Id == request.Id);
+            if (project is null) return ResultViewModel.Error("Projeto não existe!");
+            project.SetAsDeleted();
+            _context.Projects.Update(project);
+            _context.SaveChangesAsync(true);
+            return ResultViewModel.Success();
+        }
+    }
+}
