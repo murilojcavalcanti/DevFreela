cdfc4813fe393df286ee03f14c26ac0f9e5a2673
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-12 18:41:27 -0300

    Implementando o UpdateProject

---
+++ DevFreela.Application/Services/Commands/CommandsProject/UpdateProject/UpdateProjectCommand.cs
@@ -0,0 +1,18 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.UpdateProject
+{
+    public class UpdateProjectCommand:IRequest<ResultViewModel>
+    {
+        public int IdProject { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandsProject/UpdateProject/UpdateProjectHandler.cs
@@ -0,0 +1,32 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandsProject.UpdateProject
+{
+    public class UpdateProjectHandler : IRequestHandler<UpdateProjectCommand, ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public UpdateProjectHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(UpdateProjectCommand request, CancellationToken cancellationToken)
+        {
+            Project project = await _context.Projects.SingleOrDefaultAsync(p => p.Id == request.IdProject);
+            if (project is null) return ResultViewModel.Error("Projeto não existe");
+            project.Update(request.Title, request.Description, request.TotalCost);
+            _context.SaveChangesAsync();
+            return ResultViewModel.Success();
+        }
+    }
+}
