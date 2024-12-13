cef1948a84d7843f5e06107365769687953011cd
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-13 15:56:12 -0300

    Corrigindo Errro no UpdateProjectCommand

--- DevFreela.Application/Services/Commands/CommandsProject/UpdateProject/UpdateProjectCommand.cs
+++ DevFreela.Application/Services/Commands/CommandsProject/UpdateProject/UpdateProjectCommand.cs
@@ -10,6 +10,14 @@ namespace DevFreela.Application.Services.Commands.CommandsProject.UpdateProject
 {
     public class UpdateProjectCommand:IRequest<ResultViewModel>
     {
+        public UpdateProjectCommand(int idProject, string title, string description, decimal totalCost)
+        {
+            IdProject = idProject;
+            Title = title;
+            Description = description;
+            TotalCost = totalCost;
+        }
+
         public int IdProject { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
