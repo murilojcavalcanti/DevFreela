d8e503bd410c6284496cb4e59a9571402097d3f1
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-17 19:38:09 -0300

    criando inframodule

---
+++ DevFreela.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Core.Repositories.ProjectRepositories;
+using DevFreela.Infrastructure.Persistence.Repositories.ProjectRepositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddRepositories();
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            return services;
+        }
+    }
+}
