2d4aecf1f78cb4f3c2159066f423fd19966d6327
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-17 19:37:28 -0300

    criando interface de projectrepository

---
+++ DevFreela.Core/Repositories/ProjectRepositories/IProjectRepository.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Repositories.ProjectRepositories
+{
+    public interface IProjectRepository
+    {
+        Task<List<Project>> GetAll();
+        Task<Project> GetDetailsById(int id);
+        Task<Project> GetById(int id);
+        Task<bool> Exists(int id);
+        Task<int> Add(Project project);
+        Task<int> AddComment(ProjectComment project);
+        Task Update(Project project);
+        Task Delete(int id);
+
+    }
+}
