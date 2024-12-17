7d925cf39a97a8f0ece74d20392995d559eb29af
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-17 17:28:36 -0300

    Retirando models sem uso

--- DevFreela.Application/Models/projectComments/CreateProjectCommentInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevFreela.Infrastructure.Models.projectComments
-{
-    public class CreateProjectCommentInputModel
-    {
-        public string Content { get; set; }
-        public int IdProject { get; set; }
-        public int IdUser { get; set; }
-    }
-}
