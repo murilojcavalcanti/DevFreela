7f79c680537514b279d7e2255029cb179dee4544
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-19 17:08:24 -0300

    Command DeleteUserSkill

---
+++ DevFreela.Application/Services/Commands/CommandUser/DeleteUserSkill/DeleteUserSkillCommand.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+using MediatR.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.DeleteUserSkill
+{
+    public class DeleteUserSkillCommand:IRequest<ResultViewModel>
+    {
+        public DeleteUserSkillCommand(int userId, int skillId)
+        {
+            UserId = userId;
+            SkillId = skillId;
+        }
+
+        public int UserId { get; set; }
+        public int SkillId { get; set; }
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandUser/DeleteUserSkill/DeleteUserSkillHandler.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.DeleteUserSkill
+{
+    public class DeleteUserSkillHandler: IRequestHandler<DeleteUserSkillCommand,ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+        public DeleteUserSkillHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ResultViewModel> Handle(DeleteUserSkillCommand request, CancellationToken cancellationToken)
+        {
+            var userSkil = await _context.UserSkills.SingleOrDefaultAsync(p => p.IdUser == request.UserId && p.IdSkill==request.SkillId);
+            if (userSkil is null) return ResultViewModel.Error("UserSkil não existe!");
+            userSkil.SetAsDeleted();
+            _context.UserSkills.Update(userSkil);
+            _context.SaveChangesAsync(true);
+            return ResultViewModel.Success();
+        }
+    }
+}
