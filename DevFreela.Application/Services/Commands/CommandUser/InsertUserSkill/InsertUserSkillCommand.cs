5e4a4247417a3d3b035a3563d9584432e4903627
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-19 17:09:01 -0300

    Command InsertUserSkill

---
+++ DevFreela.Application/Services/Commands/CommandUser/InsertUserSkill/InsertUserSkillCommand.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.InsertUserSkill
+{
+    public class InsertUserSkillCommand:IRequest<ResultViewModel>
+    {
+        public int Id { get; set; }
+        public List<int> SkillsId { get; set; }
+
+        public InsertUserSkillCommand(List<int> skillsId, int id)
+        {
+            SkillsId = skillsId;
+            Id = id;
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandUser/InsertUserSkill/InsertUserSkillHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.InsertUserSkill
+{
+    public class InsertUserSkillHandler : IRequestHandler<InsertUserSkillCommand, ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public InsertUserSkillHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(InsertUserSkillCommand request, CancellationToken cancellationToken)
+        {
+            var UserSkills = request.SkillsId.Select(s => new UserSkill(request.Id, s)).ToList();
+            await _context.UserSkills.AddRangeAsync(UserSkills);
+            return ResultViewModel.Success();
+        }
+    }
+}
