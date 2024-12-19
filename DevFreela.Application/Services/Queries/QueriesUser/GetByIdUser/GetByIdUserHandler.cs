2c6e30c77abd16412bcca31d907c603c25257423
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-19 17:10:00 -0300

    Query GetByIdUser

---
+++ DevFreela.Application/Services/Queries/QueriesUser/GetByIdUser/GetByIdUserHandler.cs
@@ -0,0 +1,38 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Models.user;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesUser.GetByIdUser
+{
+    public class GetByIdUserHandler : IRequestHandler<GetByIdUserQuery, ResultViewModel<UserViewModel>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetByIdUserHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<UserViewModel>> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+        {
+            User? user = await _context.Users
+               .Include(u => u.Skills)
+               .ThenInclude(u => u.Skill)
+               .SingleOrDefaultAsync(u => u.Id == request.IdUser);
+            
+            if (user is null) return ResultViewModel<UserViewModel>.Error("Usuário não encontrado!");
+            
+            UserViewModel result = UserViewModel.FromEntity(user);
+            
+            return ResultViewModel<UserViewModel>.Success(result);
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Queries/QueriesUser/GetByIdUser/GetByIdUserQuey.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Models.user;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesUser.GetByIdUser
+{
+    public class GetByIdUserQuery : IRequest<ResultViewModel<UserViewModel>>
+    {
+
+        public int IdUser { get; set; }
+
+        public GetByIdUserQuery(int idUser)
+        {
+            IdUser = idUser;
+        }
+    }
+}
