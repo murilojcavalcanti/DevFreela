100d8f8d44aca70b155b64982e42f2c2d90c661e
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-19 17:08:45 -0300

    Command InsertUser

---
+++ DevFreela.Application/Services/Commands/CommandUser/InsertUser/InsertUserCommand.cs
@@ -0,0 +1,28 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.InsertUser
+{
+    public class InsertUserCommand:IRequest<ResultViewModel<int>>
+    {
+        public InsertUserCommand(string fullName, string email, DateTime birthDate)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public User ToEntity() => new User(FullName, Email, BirthDate);
+
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandUser/InsertUser/InsertUserHandler.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.InsertUser
+{
+    public class InsertUserHandler : IRequestHandler<InsertUserCommand, ResultViewModel<int>>
+    {
+        private  readonly DevFreelaDbContext _context;
+
+        public InsertUserHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(InsertUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = request.ToEntity();
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return ResultViewModel<int>.Success(user.Id);
+        }
+    }
+}
