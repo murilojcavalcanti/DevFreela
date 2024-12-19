8b8fde1be7b5cfb473fb348d9e756c300aa0e94f
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-19 17:09:40 -0300

    Query GetAllUsers

---
+++ DevFreela.Application/Services/Queries/QueriesUser/GetAllUsers/GetAllUsersHandler.cs
@@ -0,0 +1,36 @@
+﻿using Azure;
+using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Models.user;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesUser.GetAllUsers
+{
+    public class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, ResultViewModel<List<UserViewModel>>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetAllUsersHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<List<UserViewModel>>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await  _context.Users
+            .Include(u => u.Skills)
+            .ThenInclude(u => u.Skill)
+            .ToListAsync();
+            List<UserViewModel> result = users.Select(u => UserViewModel.FromEntity(u)).ToList();
+            return ResultViewModel<List<UserViewModel>>.Success(result);
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Queries/QueriesUser/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Models.user;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesUser.GetAllUsers
+{
+    public class GetAllUsersQuery : IRequest<ResultViewModel<List<UserViewModel>>>
+    {
+    }
+}
