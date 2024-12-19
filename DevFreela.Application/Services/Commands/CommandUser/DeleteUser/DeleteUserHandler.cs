217619d721b98e82e682a5d728120f79a790aa26
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-19 17:08:04 -0300

    Command DeleteUser

---
+++ DevFreela.Application/Services/Commands/CommandUser/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.DeleteUser
+{
+    public class DeleteUserHandler:IRequestHandler<DeleteUserCommand,ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+        public DeleteUserHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = _context.Users.FirstOrDefault(u=>u.Id==request.Id);
+            if (user is null) return ResultViewModel.Error("Usuario não existente!");
+            user.SetAsDeleted();
+            _context.Update(user);
+            _context.SaveChanges();
+            return ResultViewModel.Success();
+        }
+    }
+}
