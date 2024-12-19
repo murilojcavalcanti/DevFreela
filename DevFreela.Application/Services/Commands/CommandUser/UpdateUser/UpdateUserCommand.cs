a062e044f94cb8e4dd340f055cb9fb05dbc7aa8e
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-19 17:09:20 -0300

    Comand UpdateUser

---
+++ DevFreela.Application/Services/Commands/CommandUser/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<ResultViewModel>
+    {
+        public UpdateUserCommand(int userId, string fullName, string email, DateTime birthDate)
+        {
+            UserId = userId;
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public User ToEntity() => new User(FullName, Email, BirthDate);
+
+    }
+}
---
+++ DevFreela.Application/Services/Commands/CommandUser/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandUser.UpdateUser
+{
+    public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, ResultViewModel>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public UpdateUserHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = await _context.Users.SingleOrDefaultAsync(u => u.Id == request.UserId);
+            if (user == null) return ResultViewModel.Error("Usuário Inexistente ou Id Incorreto");
+            user.Update(request.FullName,request.Email,request.BirthDate);
+            _context.Users.Update(user);
+            _context.SaveChanges();
+            return ResultViewModel.Success();
+        }
+    }
+}
