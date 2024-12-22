b5a42763523662657df8d97c8f221a843e1532b8
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-22 16:00:03 -0300

    Refatorando InsertCommentProject para usar o Repository

--- DevFreela.Application/Services/Commands/CommandsProject/InsertCommentProject/InsertCommentProjectHandler.cs
+++ DevFreela.Application/Services/Commands/CommandsProject/InsertCommentProject/InsertCommentProjectHandler.cs
@@ -1,34 +1,28 @@
 ﻿using DevFreela.Application.Models;
 using DevFreela.Core.Entities;
-using DevFreela.Infrastructure.Persistence;
+using DevFreela.Core.Repositories.ProjectRepositories;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevFreela.Application.Services.Commands.CommandsProject.InsertCommentProject
 {
     public class InsertCommentProjectHandler : IRequestHandler<InsertCommentProjectCommand, ResultViewModel>
     {
 
-        private readonly DevFreelaDbContext _context;
+        private readonly IProjectRepository _repository;
 
-        public InsertCommentProjectHandler(DevFreelaDbContext context)
+        public InsertCommentProjectHandler(IProjectRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
+
 
         public async Task<ResultViewModel> Handle(InsertCommentProjectCommand request, CancellationToken cancellationToken)
         {
-            Project project = await _context.Projects.SingleOrDefaultAsync(p => p.Id == request.IdProject);
-            if (project is null) return ResultViewModel.Error("Projeto não existe!");
+            var project = await _repository.Exists(request.IdProject);
+            if (project == false) return ResultViewModel.Error("Projeto não existe!");
 
             var comment = new ProjectComment(request.Content, request.IdProject, request.IdUser);
-            _context.ProjectComments.AddAsync(comment);
-            _context.SaveChangesAsync();
+            await _repository.AddComment(comment);
 
             return ResultViewModel.Success();
 
