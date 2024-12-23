47db1bf23fcc06ef0864c8b61d7e838fdcf975ac
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-22 22:13:38 -0300

    Adicionando validação Ao Insertproject

---
+++ DevFreela.Application/Validators/ValidatorsProject/InsertProjectValidator.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Application.Services.Commands.CommandsProject.InsertProject;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Validators.ValidatorsProject
+{
+    public class InsertProjectValidator:AbstractValidator<InsertProjectCommand>
+    {
+        public InsertProjectValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .WithMessage("Não pode ser Vazio")
+                .MaximumLength(50)
+                .WithMessage("Tamanho Maximo é 50.");
+
+            RuleFor(p => p.TotalCost)
+            .GreaterThan(1000)
+            .WithMessage("O custo deve ser maior que R$1000,00") ;
+        }
+    }
+}
