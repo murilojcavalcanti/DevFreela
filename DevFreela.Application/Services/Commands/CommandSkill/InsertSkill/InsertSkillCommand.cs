0cc7a3309d280c356b5144e6be7ffeeda99a2271
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-26 19:15:38 -0300

    criando command InsertUser

---
+++ DevFreela.Application/Services/Commands/CommandSkill/InsertSkill/InsertSkillCommand.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Commands.CommandSkill.InsertSkill
+{
+    public class InsertSkillCommand : IRequest<ResultViewModel<int>>
+    {
+        public InsertSkillCommand(string description)
+        {
+            Description = description;
+        }
+
+        public string Description { get; set; }
+        public Skill ToEntity() => new Skill(Description);
+    }
+}
