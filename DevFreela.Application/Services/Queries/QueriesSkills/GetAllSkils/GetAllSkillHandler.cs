ff0b526f681d2529ee9873b9628eacb26be4234b
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-27 23:55:06 -0300

    Implementando o GetAllSkill

---
+++ DevFreela.Application/Services/Queries/QueriesSkills/GetAllSkils/GetAllSkillHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Application.Models.Skills;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesSkills.GetAllSkils
+{
+    public class GetAllUserHandler : IRequestHandler<GetAllSkillQuery, ResultViewModel<List<SkillViewModel>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetAllUserHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<List<SkillViewModel>>> Handle(GetAllSkillQuery request, CancellationToken cancellationToken)
+        {
+            var skils = _context.Skills.ToList();
+            var result = skils.Select(s => SkillViewModel.FromEntity(s)).ToList();
+
+            return ResultViewModel<List<SkillViewModel>>.Success(result);
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Queries/QueriesSkills/GetAllSkils/GetAllSkillQuery.cs
@@ -0,0 +1,16 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Application.Models.Skills;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesSkills.GetAllSkils
+{
+    public class GetAllSkillQuery : IRequest<ResultViewModel<List<SkillViewModel>>
+    {
+        
+    }
+}
