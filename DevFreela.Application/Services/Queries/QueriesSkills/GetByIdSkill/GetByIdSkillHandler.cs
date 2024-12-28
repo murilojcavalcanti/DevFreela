cbd0cb06e96208b58d9bdd0a7d470e9e079881f7
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-27 23:55:25 -0300

    Implementando o GetByIdSkill

---
+++ DevFreela.Application/Services/Queries/QueriesSkills/GetByIdSkill/GetByIdSkillHandler.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Models.Skills;
+using DevFreela.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Services.Queries.QueriesSkills.GetByIdSkill
+{
+    public class GetByIdSkillHandler : IRequestHandler<GetByIdSkillQuery, ResultViewModel<SkillViewModel>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetByIdSkillHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<SkillViewModel>> Handle(GetByIdSkillQuery request, CancellationToken cancellationToken)
+        {
+            var skill = await _context.Skills.SingleOrDefaultAsync(s => s.Id == request.id);
+            var result = SkillViewModel.FromEntity(skill);
+            return ResultViewModel<SkillViewModel>.Success(result);
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Queries/QueriesSkills/GetByIdSkill/GetByIdSkillQuery.cs
@@ -0,0 +1,21 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Application.Models.Skills;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesSkills.GetByIdSkill
+{
+    public class GetByIdSkillQuery: IRequest<ResultViewModel<SkillViewModel>>
+    {
+        public int id { get; set; }
+
+        public GetByIdSkillQuery(int id)
+        {
+            this.id = id;
+        }
+    }
+}
