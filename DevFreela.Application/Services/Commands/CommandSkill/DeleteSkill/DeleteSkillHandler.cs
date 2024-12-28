1b2d2d1b85ac4f16411de21e88233b258fcdd042
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-27 23:54:31 -0300

    implementadno handler DeleteSkil

--- DevFreela.Application/Services/Commands/CommandSkill/DeleteSkill/DeleteSkillHandler.cs
+++ DevFreela.Application/Services/Commands/CommandSkill/DeleteSkill/DeleteSkillHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,22 @@ using System.Threading.Tasks;
 
 namespace DevFreela.Application.Services.Commands.CommandSkill.DeleteSkill
 {
-    internal class DeleteSkillHandler
+    public class DeleteSkillHandler : IRequestHandler<DeleteSkillCommand, ResultViewModel>
     {
+        private readonly DevFreelaDbContext _context;
+
+        public DeleteSkillHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel> Handle(DeleteSkillCommand request, CancellationToken cancellationToken)
+        {
+            Skill skill = _context.Skills.SingleOrDefault(s => s.Id == request.Id);
+            skill.SetAsDeleted();
+            _context.Update(skill);
+            _context.SaveChanges();
+            return ResultViewModel.Success();
+        }
     }
 }
