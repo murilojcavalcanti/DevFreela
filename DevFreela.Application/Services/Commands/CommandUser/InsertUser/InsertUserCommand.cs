78489e88b16902282f62b83622b678a3b7d8a8b0
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-30 18:37:23 -0300

    Atualizando o command

--- DevFreela.Application/Services/Commands/CommandUser/InsertUser/InsertUserCommand.cs
+++ DevFreela.Application/Services/Commands/CommandUser/InsertUser/InsertUserCommand.cs
@@ -11,18 +11,22 @@ namespace DevFreela.Application.Services.Commands.CommandUser.InsertUser
 {
     public class InsertUserCommand:IRequest<ResultViewModel<int>>
     {
-        public InsertUserCommand(string fullName, string email, DateTime birthDate)
+        public InsertUserCommand(string fullName, string email, DateTime birthDate, string role, string password)
         {
             FullName = fullName;
             Email = email;
             BirthDate = birthDate;
+            Role = role;
+            Password = password;
         }
 
         public string FullName { get; set; }
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
+        public string Role { get; set; }
+        public string Password { get; set; }
 
-        public User ToEntity() => new User(FullName, Email, BirthDate);
+        public User ToEntity() => new User(FullName, Email, BirthDate,Password,Role);
 
     }
 }
