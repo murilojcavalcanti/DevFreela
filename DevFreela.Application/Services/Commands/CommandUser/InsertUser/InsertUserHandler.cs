5f7461149ad727338831aaae383dc3ce2ae03a08
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-31 17:50:38 -0300

    implementando no handler

--- DevFreela.Application/Services/Commands/CommandUser/InsertUser/InsertUserHandler.cs
+++ DevFreela.Application/Services/Commands/CommandUser/InsertUser/InsertUserHandler.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.Application.Models;
 using DevFreela.Core.Entities;
+using DevFreela.Core.Services.Auth;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -14,15 +15,18 @@ namespace DevFreela.Application.Services.Commands.CommandUser.InsertUser
     public class InsertUserHandler : IRequestHandler<InsertUserCommand, ResultViewModel<int>>
     {
         private  readonly DevFreelaDbContext _context;
-
-        public InsertUserHandler(DevFreelaDbContext context)
+        private readonly IAuthService _authService;
+        public InsertUserHandler(DevFreelaDbContext context, IAuthService authService)
         {
             _context = context;
+            _authService = authService;
         }
 
         public async Task<ResultViewModel<int>> Handle(InsertUserCommand request, CancellationToken cancellationToken)
         {
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
             User user = request.ToEntity();
+            user.Password = passwordHash;
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return ResultViewModel<int>.Success(user.Id);
