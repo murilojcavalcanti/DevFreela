07b185dcd8845e9f2148d0d91922cdc7c3eceaca
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-31 17:50:10 -0300

    Criando ComputeHash

--- DevFreela.Application/Services/AuthServices/AuthService.cs
+++ DevFreela.Application/Services/AuthServices/AuthService.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +24,7 @@ namespace DevFreela.Application.Services.AuthServices
         public string GenerateToken(string email, string Role)
         {
             var Issuer = _configuration["Jwt:Issuer"];  
-            var Audience = _configuration["Jwt:Audience"];  
+            var Audience = _configuration["Jwt:Audience"];
             var Key = _configuration["Jwt:Key"];
             var Skey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key));
             var credentials = new SigningCredentials(Skey, SecurityAlgorithms.HmacSha256);
@@ -37,5 +38,21 @@ namespace DevFreela.Application.Services.AuthServices
             var tokenString = tokenHandler.WriteToken(token);
             return tokenString;
          }
+
+        string ComputeSha256Hash(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("X2"));
+                    
+                }
+           
+                return builder.ToString();
+            }
+        }
     }
 }
