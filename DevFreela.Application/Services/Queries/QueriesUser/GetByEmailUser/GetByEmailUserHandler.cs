bf0505b407eeebfa1be2f712f5c57f20080f1473
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-31 19:03:40 -0300

    Criando query de busca por email

---
+++ DevFreela.Application/Services/Queries/QueriesUser/GetByEmailUser/GetByEmailUserHandler.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Models.user;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Queries.QueriesUser.GetByEmailUser
+{
+    public class GetByEmailUserHandler : IRequestHandler<GetByEmailUserQuery, ResultViewModel<User>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetByEmailUserHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<User>> Handle(GetByEmailUserQuery request, CancellationToken cancellationToken)
+        {
+            User? user = await _context.Users
+               .Include(u => u.Skills)
+               .ThenInclude(u => u.Skill)
+               .SingleOrDefaultAsync(u => u.Email == request.EmailUser);
+            
+            if (user is null) return ResultViewModel<User>.Error("Usuário não encontrado!");
+            
+            
+            return ResultViewModel<User>.Success(user);
+        }
+    }
+}
---
+++ DevFreela.Application/Services/Queries/QueriesUser/GetByEmailUser/GetByEmailUserQuery.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using MediatR;
+
+namespace DevFreela.Application.Services.Queries.QueriesUser.GetByEmailUser
+{
+    public class GetByEmailUserQuery : IRequest<ResultViewModel<User>>
+    {
+
+        public string EmailUser { get; set; }
+
+        public GetByEmailUserQuery( string emailUser)
+        {
+            EmailUser = emailUser;
+        }
+    }
+}
