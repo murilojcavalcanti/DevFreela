6922412f4ca8ec05cab27fc7c0134209cbcc6ae5
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-03 21:00:55 -0300

    Criando loginModel

---
+++ DevFreela.Application/Models/user/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DevFreela.Application.Models.user
+{
+    public class LoginInputModel
+    {
+        public LoginInputModel(string email, string token)
+        {
+            Email = email;
+            Token = token;
+        }
+
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+
+    public class loginViewModel
+    {
+        public string Token { get; set; }
+
+        public loginViewModel(string token)
+        {
+            Token = token;
+        }
+    }
+
+}
