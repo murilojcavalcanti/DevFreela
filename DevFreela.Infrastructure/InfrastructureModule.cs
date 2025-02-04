a85bf3b9ab185f42ef0111a9cd4219d7b3575f3a
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-03 21:04:08 -0300

    Aplicando authservice no container di

--- DevFreela.Infrastructure/InfrastructureModule.cs
+++ DevFreela.Infrastructure/InfrastructureModule.cs
@@ -1,9 +1,14 @@
 ﻿using DevFreela.Core.Repositories.ProjectRepositories;
+using DevFreela.Core.Services.Auth;
+using DevFreela.Infrastructure.AuthServices;
 using DevFreela.Infrastructure.Persistence;
 using DevFreela.Infrastructure.Persistence.Repositories.ProjectRepositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace DevFreela.Infrastructure
 {
@@ -12,7 +17,8 @@ namespace DevFreela.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
         {
             services.AddRepositories()
-                .AddData(configuration);
+                .AddData(configuration)
+                .AddAuth(configuration);
 
             return services;
         }
@@ -27,6 +33,25 @@ namespace DevFreela.Infrastructure
         {
             services.AddScoped<IProjectRepository, ProjectRepository>();
             return services;
+        }
+        public static IServiceCollection AddAuth(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddScoped<IAuthService, AuthService>();
+            service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                };
+            });
+            return service;
         }
     }
 }
