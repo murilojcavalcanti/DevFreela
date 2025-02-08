1c55570602037be8ece3969d084f1c769cfde6df
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-08 14:55:22 -0300

    Atualizando userviewModel

--- DevFreela.Application/Models/user/UserViewModel.cs
+++ DevFreela.Application/Models/user/UserViewModel.cs
@@ -4,23 +4,23 @@ namespace DevFreela.Infrastructure.Models.user
 {
     public class UserViewModel
     {
-        public UserViewModel(string fullName, string email, DateTime birthDate, List<string> skills)
+        public UserViewModel(string fullName, string email, DateTime birthDate, List<string> skills, string role)
         {
             FullName = fullName;
             Email = email;
             BirthDate = birthDate;
             Skills = skills;
+            Role = role;
         }
-
         public string FullName { get; private set; }
         public string Email { get; private set; }
         public DateTime BirthDate { get; private set; }
+        public string Role { get; set; }
         public List<string> Skills { get;private set; }
-
         public static UserViewModel FromEntity(User user)
         {
             var skills = user.Skills.Select(s => s.Skill.Description).ToList();
-            return new UserViewModel(user.FullName, user.Email, user.BirthDate,skills );
+            return new UserViewModel(user.FullName, user.Email, user.BirthDate,skills,user.Role );
         }
     }
 }
